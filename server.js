98a9d6f89ba7a8a35ac9f433ab90b1fbccaa47cc
gbell <gbellamkonda@github.com>
2017-12-21 17:33:27 -0500

    modification to look into / instead of public

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http  = require('http');
 var app      = express();
  
 app.configure(function() {
-	app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
+	app.use(express.static(__dirname + '/')); 		// set the static files location /public/img will be /img for users
 	app.use(express.logger('dev')); 						// log every request to the console
 	app.use(express.bodyParser()); 							// pull information from html in POST
 	app.use(express.methodOverride()); 						// simulate DELETE and PUT
